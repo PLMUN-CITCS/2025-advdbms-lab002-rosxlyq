ada1098724a293a260b26ec38f5e39ff513e6113
rosxlyq <deveyrarosalyn05@gmail.com>
2025-02-10 15:49:29 +0800

    Completed assignment: Created tables and inserted data

--- scripts/02_insert_data.sql
+++ scripts/02_insert_data.sql
@@ -13,3 +13,4 @@
 
     2. Place your insert queries below this comment
 */
+INSERT INTO `tblstudents`(`student_name`, `student_email`, `student_phone`) VALUES ('John Doe','johndoe@gmail.com','123-456-7890'), ('Jane Doe','janedoe@gmail.com','123-456-7890');
